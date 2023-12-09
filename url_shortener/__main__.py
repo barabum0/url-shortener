a1b6b712a49b156780e1a10ac7866ead1226c725
roman mac <barabum@duck.com>
2023-12-09 16:13:20 +0300

    :recycle: refactor: move FastAPI app instance to module level

--- url_shortener/__main__.py
+++ url_shortener/__main__.py
@@ -5,15 +5,16 @@ from loguru import logger
 from url_shortener.routers import router as main_router
 from url_shortener.tools.logs import configure_logger, get_uvicorn_log_config
 
+app = FastAPI()
+
 
 def main():
     configure_logger()
     logger.info("Starting...")
 
-    app = FastAPI()
     app.include_router(main_router)
 
-    uvicorn.run(app, log_config=get_uvicorn_log_config())
+    uvicorn.run("url_shortener.__main__:app", log_config=get_uvicorn_log_config())
 
 
 if __name__ == "__main__":
