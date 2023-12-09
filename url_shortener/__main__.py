e06e7aea08532754718528757f3fb26a3c765e16
roman mac <barabum@duck.com>
2023-12-10 01:17:15 +0300

    ✨ feat: add CORS middleware to FastAPI application

--- url_shortener/__main__.py
+++ url_shortener/__main__.py
@@ -3,12 +3,18 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi_responses import custom_openapi
 from loguru import logger
+from starlette.middleware.cors import CORSMiddleware
 
 from url_shortener.routers import router as main_router
 from url_shortener.tools.logs import configure_logger, get_uvicorn_log_config
 
 app = FastAPI()
 app.openapi = custom_openapi(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
 
 
 def main():
