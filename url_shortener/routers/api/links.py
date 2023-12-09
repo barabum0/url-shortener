50b9dfec0d0afaa74a5daab365eb2be1c6b54810
roman mac <barabum@duck.com>
2023-12-09 17:53:32 +0300

    :sparkles: feat: initialize database connection in links API router

--- url_shortener/routers/api/links.py
+++ url_shortener/routers/api/links.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
+from url_shortener.database import create_db_client
+from url_shortener.database.database import Database
+
 router = APIRouter(prefix="/links", tags=["Links"])
+db = Database(create_db_client())
