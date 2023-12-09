edd0e4377d50f4f89a1b1a3e1cdde47c5a00933a
roman mac <barabum@duck.com>
2023-12-09 21:55:40 +0300

    :recycle: refactor(database): Use BaseDatabaseID with custom id generation in Links table

--- url_shortener/database/database.py
+++ url_shortener/database/database.py
@@ -1,6 +1,8 @@
+from uuid import uuid4
+
 from motor.core import AgnosticClient
 
-from url_shortener.database import BaseDatabase
+from url_shortener.database import BaseDatabaseID
 from url_shortener.types.links import Link
 
 
@@ -8,4 +10,10 @@ class Database:
     def __init__(self, client: AgnosticClient):
         self.client = client
 
-        self.Links = BaseDatabase[Link]("links", self.client, Link)
+        self.Links = BaseDatabaseID[Link](
+            "links",
+            self.client,
+            Link,
+            id_key="link_hash",
+            generate_id_function=lambda: str(uuid4())[:6],
+        )
